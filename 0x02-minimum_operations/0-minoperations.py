26b81f0f3fe7905028307d5f9241010741b92d96
Abokiey <sweneysam@gmail.com>
2024-03-21 11:36:38 +0300

    0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,15 +5,16 @@
 def minOperations(n):
     """calculate the fewest no. of operations needed"""
     if n <= 1:
-      return 0
-    
+        return 0
+
     operations = 0
     i = 2
-    
+
     while n > 1:
         if n % i == 0:
             operations += i
             n = n / i
         else:
             i += 1
+
     return operations
