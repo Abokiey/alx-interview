1e62659fad46de625a04c9cafeff9ace7ef67e14
Abokiey <sweneysam@gmail.com>
2024-05-02 22:21:53 +0300

    0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,16 +8,15 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    coins.sort(reverse=True)
-    sum = 0
-    i = 0
-    count = 0
-    num_coins = len(coins)
-    while sum < total and i < num_coins:
-        while coins[i] <= total - sum:
-            sum += coins[i]
-            counter += 1
-            if sum == total:
-                return count
-        i += 1
-    return -1
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
